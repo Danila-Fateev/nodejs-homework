6a1aa688752db8b794e729de73a423ce161c042f
Вадим Фатеев <fateevdanon@gmail.com>
2022-10-11 21:32:58 +0200

    Update updateStatusContact.js

--- controllers/contacts/updateStatusContact.js
+++ controllers/contacts/updateStatusContact.js
@@ -2,6 +2,7 @@ const { Contact } = require("../../models/contact");
 
 const updateStatusContact = async (contactId, body) => {
   if (!body) throw new Error({ message: "missing field favorite" });
+
   const contacts = await Contact.findByIdAndUpdate(contactId, body, {
     new: true,
   });
