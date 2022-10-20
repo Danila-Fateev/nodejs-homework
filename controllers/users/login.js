90a46ca1c99801ccc1152cd900e82817e0161662
Вадим Фатеев <fateevdanon@gmail.com>
2022-10-20 15:10:46 +0200

    step 2

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -5,8 +5,11 @@ const { SECRET_KEY } = process.env;
 
 const login = async (body) => {
   const { email, password } = body;
-  const foundUserByEmail = User.find({ email });
-  const correctPassword = bcrypt.compare(password, foundUserByEmail.password);
+  const [foundUserByEmail] = await User.find({ email });
+  const correctPassword = await bcrypt.compare(
+    password,
+    foundUserByEmail.password
+  );
   if (!foundUserByEmail || !correctPassword) {
     throw new Error({ status: 401, message: "Email or password is wrong" });
   }
@@ -14,7 +17,8 @@ const login = async (body) => {
     id: foundUserByEmail._id,
     email: foundUserByEmail.email,
   };
-  const newToken = jwt.sign(payload, SECRET_KEY);
+
+  const newToken = await jwt.sign(payload, SECRET_KEY);
   await foundUserByEmail.update({ token: newToken });
   return newToken;
 };
